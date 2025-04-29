1f1db9d4a50e5a99d492b409fbecf893b973a279
Mohamed Hamed <mdhamed.dev@gmail.com>
2025-04-29 16:05:00 +0200

    Initial commit: Project setup

---
+++ src/main/java/com/mdhamed/weatherly/WeatherlyApplication.java
@@ -0,0 +1,13 @@
+package com.mdhamed.weatherly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdhamed/weatherly/WeatherlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdhamed.weatherly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
